31cb5568c868102260493173115ed06fc6cc0d23
Matthew Dizon <dominic.dizon@obf.ateneo.edu>
2022-08-18 15:56:57 +0800

    updated home page to be empty

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ function Home() {
   return (
     <Layout>
       <h1>Finance Tracking</h1>
-      <h2>Who?</h2>
+      {/* <h2>Who?</h2>
       <a href="https://matthewdizon.com" target="_blank" rel="noreferrer">
         Matthew Dizon
       </a>
       <h2>What problem are we solving?</h2>
-      <h2>What are the features?</h2>
+      <h2>What are the features?</h2> */}
     </Layout>
   );
 }
