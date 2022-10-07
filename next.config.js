26e253a4bd25cd13d104aef253e469867d5603aa
Matthew Dizon <dominic.dizon@obf.ateneo.edu>
2022-10-08 04:21:10 +0800

    test enabling cors in next config

--- next.config.js
+++ next.config.js
@@ -19,8 +19,30 @@ module.exports = () => {
       },
     ];
   };
+  const headers = async () => {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  };
   return {
     rewrites,
+    headers,
     images: {
       domains: ["localhost", "lh3.googleusercontent.com"], // <== Domain name
     },
