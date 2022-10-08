e03936deb895d232a89bcac281a97956b7caa09a
Matthew Dizon <dominic.dizon@obf.ateneo.edu>
2022-10-09 02:36:30 +0800

    updated config with only headers

--- next.config.js
+++ next.config.js
@@ -1,24 +1,4 @@
 module.exports = () => {
-  const rewrites = () => {
-    return [
-      {
-        source: "/api/transactions",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_SERVER}/api/transactions`,
-      },
-      {
-        source: "/api/transactions/:id",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_SERVER}/api/transactions/:id`,
-      },
-      {
-        source: "/api/wallets",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_SERVER}/api/wallets`,
-      },
-      {
-        source: "/api/wallets/:id",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_SERVER}/api/wallets/:id`,
-      },
-    ];
-  };
   const headers = async () => {
     return [
       {
@@ -41,7 +21,7 @@ module.exports = () => {
     ];
   };
   return {
-    rewrites,
+    // rewrites,
     headers,
     images: {
       domains: ["localhost", "lh3.googleusercontent.com"], // <== Domain name
